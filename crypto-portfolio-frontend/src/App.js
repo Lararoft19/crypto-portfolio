b2f2fe6d192eae6bb70c7d463e9840d5279b1d6a
Tira <amydelreyz@gmail.com>
2024-06-25 02:51:12 +0800

    recommiting files

--- crypto-portfolio-frontend/src/App.js
+++ crypto-portfolio-frontend/src/App.js
@@ -1,23 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function App() {
-  const [portfolios, setPortfolios] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:4000/api/portfolio')
-      .then(response => response.json())
-      .then(data => setPortfolios(data))
-      .catch(error => console.error('Error:', error));
-  }, []);
-
   return (
-    <div className="Crypto Portfolio App">
-      <h1>Crypto Portfolio</h1>
-      <ul>
-        {portfolios.map((portfolio) => (
-          <li key={portfolio._id}>{portfolio.name}: {portfolio.value}</li>
-        ))}
-      </ul>
+    <div className="App">
+      <header className="App-header">
+        <h1>Crypto Portfolio</h1>
+      </header>
     </div>
   );
 }
